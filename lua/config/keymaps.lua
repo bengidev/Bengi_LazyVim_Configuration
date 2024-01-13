8b7030b639aa60455e73c8771ed4c363755da63f
B e n g i <bambang.trd17@gmail.com>
2024-01-13 12:12:47 +0700

    refactor: add alternative to select normal mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Use map to aliases `vim.keymap.set`
+local map = vim.keymap.set
+
+-- Change into normal mode
+map("i", "jj", "<Esc>")
+map("i", "jk", "<Esc>")
