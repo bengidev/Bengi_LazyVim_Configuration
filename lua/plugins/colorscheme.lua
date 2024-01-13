a8e43d853ed2641125c6fc65316d6542bd59b084
B e n g i <bambang.trd17@gmail.com>
2024-01-13 14:53:49 +0700

    feat: change colorscheme into Catppuccin

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,66 @@
+return {
+
+  -- Add Catppuccin plugin
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    lazy = true,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha", -- latte, frappe, macchiato, mocha
+        background = { -- :h background
+          light = "latte",
+          dark = "mocha",
+        },
+        transparent_background = true, -- disables setting the background color.
+        show_end_of_buffer = true, -- shows the '~' characters after the end of buffers.
+        opts = {
+          integrations = {
+            aerial = true,
+            alpha = true,
+            cmp = true,
+            dashboard = true,
+            flash = true,
+            gitsigns = true,
+            headlines = true,
+            illuminate = true,
+            indent_blankline = { enabled = true },
+            leap = true,
+            lsp_trouble = true,
+            mason = true,
+            markdown = true,
+            mini = true,
+            native_lsp = {
+              enabled = true,
+              underlines = {
+                errors = { "undercurl" },
+                hints = { "undercurl" },
+                warnings = { "undercurl" },
+                information = { "undercurl" },
+              },
+            },
+            navic = { enabled = true, custom_bg = "lualine" },
+            neotest = true,
+            neotree = true,
+            noice = true,
+            notify = true,
+            semantic_tokens = true,
+            telescope = true,
+            treesitter = true,
+            treesitter_context = true,
+            which_key = true,
+          },
+        },
+      })
+    end,
+  },
+
+  -- Configure LazyVim to load Catppuccin colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
