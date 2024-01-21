932f98d5d4a22b7b08458dafd81342cdaa4965fe
B e n g i <bambang.trd17@gmail.com>
2024-01-21 12:04:52 +0700

    refactor: add required modules to load when setup

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,7 +14,8 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
-    { import = "plugins.xcode" },
+    { import = "plugins.swift" },
+    { import = "plugins.java" },
     { import = "plugins" },
   },
   defaults = {
