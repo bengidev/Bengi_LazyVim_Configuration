6a88fe2689282df2ae6ab80dae594f8b9fd33d2a
B e n g i <bambang.trd17@gmail.com>
2024-01-21 07:21:16 +0700

    fix: load swift lsp from inside XCode directories

--- lua/plugins/xcode/nvim-lspconfig.lua
+++ lua/plugins/xcode/nvim-lspconfig.lua
@@ -1,35 +1,35 @@
----@diagnostic disable: missing-fields
-
-local util = require("lspconfig.util")
 local cmp_nvim_lsp = require("cmp_nvim_lsp")
-local capabilities = cmp_nvim_lsp.default_capabilities()
 local opts = { noremap = true, silent = true }
-local on_attach = function(_, bufnr)
-  opts.buffer = bufnr
-
-  opts.desc = "Show line diagnostics"
-  vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
-
-  opts.desc = "Show documentation for what is under cursor"
-  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-end
 
 return {
   "neovim/nvim-lspconfig",
-  event = { "BufReadPre", "BufNewFile" },
   opts = {
-    setup = {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      cmd = {
-        "/Applications/Xcode-15.2.0.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/sourcekit-lsp",
+    servers = {
+      sourcekit = {
+        cmd = {
+          "/Applications/Xcode-15.2.0.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/sourcekit-lsp",
+        },
+        root_dir = function(filename, _)
+          local util = require("lspconfig.util")
+
+          return util.root_pattern("buildServer.json")(filename)
+            or util.root_pattern("*.xcodeproj", "*.xcworkspace")(filename)
+            or util.find_git_ancestor(filename)
+            or util.root_pattern("Package.swift")(filename)
+        end,
+        capabilities = {
+          cmp_nvim_lsp.default_capabilities(),
+        },
+        on_attach = function(_, bufnr)
+          opts.buffer = bufnr
+
+          opts.desc = "Show line diagnostics"
+          vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+
+          opts.desc = "Show documentation for what is under cursor"
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        end,
       },
-      root_dir = function(filename, _)
-        return util.root_pattern("buildServer.json")(filename)
-          or util.root_pattern("*.xcodeproj", "*.xcworkspace")(filename)
-          or util.find_git_ancestor(filename)
-          or util.root_pattern("Package.swift")(filename)
-      end,
     },
   },
 }
