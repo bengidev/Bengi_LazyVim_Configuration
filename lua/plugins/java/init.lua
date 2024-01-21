9cdc4fb1a3c4583e46f1ae90c2541f994f2c7433
B e n g i <bambang.trd17@gmail.com>
2024-01-21 12:05:17 +0700

    refactor: add java module inside plugins module

---
+++ lua/plugins/java/init.lua
@@ -0,0 +1,28 @@
+---@diagnostic disable: missing-fields
+
+return {
+  "nvim-java/nvim-java",
+  name = "nvim-java",
+  config = function()
+    require("java").setup()
+    require("lspconfig").jdtls.setup({})
+  end,
+  dependencies = {
+    "nvim-java/lua-async-await",
+    "nvim-java/nvim-java-core",
+    "nvim-java/nvim-java-test",
+    "nvim-java/nvim-java-dap",
+    "MunifTanjim/nui.nvim",
+    "neovim/nvim-lspconfig",
+    "mfussenegger/nvim-dap",
+    {
+      "williamboman/mason.nvim",
+      opts = {
+        registries = {
+          "github:nvim-java/mason-registry",
+          "github:mason-org/mason-registry",
+        },
+      },
+    },
+  },
+}
