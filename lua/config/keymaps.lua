636a221c3e863d07d9f8a42582a4fbd36f111af2
B e n g i <bambang.trd17@gmail.com>
2024-01-25 05:50:02 +0700

    refactor: change Control <C> into Meta <A> keys

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -19,3 +19,9 @@ keymap("x", "J", ":m '>+1<CR>gv=gv", opts)
 keymap("x", "K", ":m '<-2<CR>gv=gv", opts)
 keymap("x", "<M-j>", ":m '>+1<CR>gv=gv", opts)
 keymap("x", "<M-k>", ":m '<-2<CR>gv=gv", opts)
+
+-- Resize window using meta or <option> arrow keys
+keymap("n", "<A-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+keymap("n", "<A-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+keymap("n", "<A-Left>", "<cmd>vertical resize +2<cr>", { desc = "Decrease window width" })
+keymap("n", "<A-Right>", "<cmd>vertical resize -2<cr>", { desc = "Increase window width" })
