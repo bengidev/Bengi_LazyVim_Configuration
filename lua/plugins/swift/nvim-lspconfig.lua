bd41938dac762e47b9ee3377f3a18668dab771cb
B e n g i <bambang.trd17@gmail.com>
2024-02-07 16:12:30 +0700

    fix: integrate XCode with NeoVim using buildServer

--- lua/plugins/swift/nvim-lspconfig.lua
+++ lua/plugins/swift/nvim-lspconfig.lua
@@ -16,7 +16,8 @@ return {
         root_dir = function(filename, _)
           local util = require("lspconfig.util")
 
-          return util.root_pattern("*.xcodeproj", "*.xcworkspace")(filename)
+          return util.root_pattern("buildServer.json")(filename)
+            or util.root_pattern("*.xcodeproj", "*.xcworkspace")(filename)
             or util.find_git_ancestor(filename)
             or util.root_pattern("Package.swift")(filename)
             or util.root_pattern("*.swift")(filename)
